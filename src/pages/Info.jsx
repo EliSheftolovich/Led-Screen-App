efcc5f923843ddb945bb88c9420a2e62c5ae99c4
Eli Sheftolovich <elishafto@gmail.com>
2021-01-29 22:11:29 +0200

    info search box added

--- src/pages/Info.jsx
+++ src/pages/Info.jsx
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
-import { Table } from 'react-bootstrap';
+import { Col, Form, Row, Table } from 'react-bootstrap';
 import Like from '../comp/common/like';
 import { getArticles } from '../data/articles';
 
 class Info extends Component {
   state={
-    articles: getArticles()
+    articles: getArticles(),
+    searchQuery: ""
   }
 
-  handleLike = (article) => {
+  handleSearch = e => {
+    this.setState({searchQuery: e.target.value})
+  }
+
+  handleLike = article => {
     const articles = [...this.state.articles];
     // console.log(articles);
     const index = articles.indexOf(article);
@@ -18,10 +23,17 @@ class Info extends Component {
     articles[index].liked = !articles[index].liked;
     // console.log(articles[index].liked);
     this.setState({articles});
-
   }
 
+
   render() { 
+    const {articles, searchQuery} =this.state;
+
+    const currentArticles = (searchQuery.length === 0)
+    ?  articles
+    :articles.filter(article => (article.title.startsWith(searchQuery))
+     || (article.text.includes(searchQuery)));
+
     return ( 
     <div>
       <h3>info with Search box</h3>
@@ -31,21 +43,29 @@ class Info extends Component {
           <tr>
             <th>נושא</th>
             <th></th>
-            <th></th>
+            <th>
+              <Row>
+                <Col lg={4} sm={8}>
+                <Form.Control type="text" placeholder="חפש..."
+                value={this.state.searchQuery} onChange={this.handleSearch}
+                />
+                </Col>
+              </Row>
+            </th>
             <th>קטגוריה</th>
           </tr>
         </thead>
         <tbody>
-          {this.state.articles.map(article => (
-          <tr key={article._id}>
-            <td>{article.title}</td>
-            <td><Like 
-            liked={article.liked} 
-            onClick={()=>this.handleLike(article)}/></td>
-            <td>{article.text}</td>
-            <td>{article.subject}</td>
-          </tr>
-          ))}
+      {currentArticles.map(article => (
+      <tr key={article._id}>
+        <td>{article.title}</td>
+        <td><Like 
+        liked={article.liked} 
+        onClick={()=>this.handleLike(article)}/></td>
+        <td>{article.text}</td>
+        <td>{article.subject}</td>
+      </tr>
+      ))} 
         </tbody>
       </Table>
 
